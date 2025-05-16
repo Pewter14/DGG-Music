74c93448b92cbb6611bd532eed7ca89a1420c858
Pewter14 <ianmachado09@gmail.com>
2025-05-16 18:42:47 -0300

    Organizei o login js e ajeitei o Link do html com js e css

--- Main/Script.js
+++ Main/Script.js
@@ -1,14 +1,1 @@
-const btnLogin = document.getElementById('tabLogin');
-const btnCadastro = document.getElementById('tabCadastro');
-const slider = document.getElementById('slider');
-const forms = document.getElementById('formsContainer');
 
-btnLogin.addEventListener('click', () => {
-  forms.style.transform = 'translateX(0%)';
-  slider.style.left = '0%';
-});
-
-btnCadastro.addEventListener('click', () => {
-  forms.style.transform = 'translateX(-50%)';
-  slider.style.left = '50%';
-});
