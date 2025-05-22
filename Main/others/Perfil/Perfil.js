475b8ba8b3cffcc479ebccacc219c12587c0c7c8
Pewter14 <ianmachado09@gmail.com>
2025-05-22 18:25:41 -0300

    me mata pfv

---
+++ Main/others/Perfil/Perfil.js
@@ -0,0 +1,26 @@
+// script.js
+
+function editProfile() {
+  document.querySelector('.edit-form').classList.remove('hidden');
+}
+
+function saveChanges() {
+  const nameInput = document.getElementById('name-input');
+  const photoInput = document.getElementById('photo-input');
+  const profileName = document.getElementById('profile-name');
+  const profilePic = document.getElementById('profile-pic');
+
+  if (nameInput.value) {
+    profileName.textContent = nameInput.value;
+  }
+
+  if (photoInput.files && photoInput.files[0]) {
+    const reader = new FileReader();
+    reader.onload = function (e) {
+      profilePic.src = e.target.result;
+    };
+    reader.readAsDataURL(photoInput.files[0]);
+  }
+
+  document.querySelector('.edit-form').classList.add('hidden');
+}
